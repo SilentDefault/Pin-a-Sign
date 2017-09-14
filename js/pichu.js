035966992e9f63e93379999c3275c78e245ab16c
SilentDefault <silentdefault@gmail.com>
2017-09-13 21:40:33 -0500

    te4

--- js/pichu.js
+++ js/pichu.js
@@ -16,14 +16,12 @@ var frases = [
     "Im fine :')"
 ];
 function sw() {//serviceworker register
-    if (navigator.serviceWorker.controller) {
-        console.log('[PWA Builder] active service worker found, no need to register')
-    } else {
+    if ('serviceWorker' in navigator) {
         //Register the ServiceWorker
-        navigator.serviceWorker.register('sw.js', {
-            scope: './'
-        }).then(function (reg) {
-            console.log('Service worker has been registered for scope:' + reg.scope);
+        navigator.serviceWorker.register('./sw.js').then(function (reg) {
+            console.log('Successfully registered service worker', reg);
+        }).catch(function (err) {
+            console.warn('Error whilst registering service worker', err);
         });
     }
 }
@@ -79,16 +77,16 @@ function loadSigns() {
 }
 
 function eleResize() {
-    $('signs').height($(document).height() - $('textarea').height());
+    $('signs').height($(window).height() - ($('editor').height() + (navigator.onLine ? 0 : $('offline').height())));
 }
 
 function txtArea() {
     var $this = $('textarea');
 
-    if ($this.val().length == 255) {
+    if ($this.val().length >= 255) {
         $this.val($this.val().substring(0, 255));
     }
-    if ($('textarea').val()!='') {
+    if ($('textarea').val() != '') {
         $('button').removeAttr('disabled');
         $('label').removeAttr('hidden');
         $('label').text($('textarea').val().length + '/255');
@@ -106,9 +104,16 @@ function txtArea() {
 $(document).ready(function () {
     sw();
     eleResize();
-    $('textarea').keydown(function () {
-        txtArea();
+    console.log($('textarea').height() + 'px');
+    $('textarea').keyup(function () { txtArea(); });
+    $('offline').click(function () {
+        $('textarea').removeAttr('disabled')
     });
+    if (navigator.onLine) {
+        workOnLine();
+    } else {
+        workOffLine();
+    }
 });
 
 $(window).resize(function () {
@@ -132,3 +137,28 @@ Lon: -error-`);
 navigator.geolocation.watchPosition(success, error, {
     timeout: 1000
 });
+
+
+function workOnLine() {
+    console.log("You are online");
+    $('offline').css('z-index', '-1');
+    $('textarea').removeAttr('disabled').keyup();
+    eleResize();
+}
+
+function workOffLine() {
+    console.log("You are offline");
+    $('offline').css('z-index', '1');
+    $('signs').height($(document).height() - ($('editor').height() + $('offline').height()));
+    $('offline').css('bottom', $('editor').height() + 'px');
+    $('textarea').attr('disabled', '');
+    $('button').attr('disabled', '');
+}
+
+window.addEventListener('online', function (e) {
+    workOnLine();
+}, false);
+
+window.addEventListener('offline', function (e) {
+    workOffLine();
+}, false);
