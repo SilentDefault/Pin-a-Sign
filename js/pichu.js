0a64a6d02ee96ecd56bc4356c5bb529f889bd5f9
SilentDefault <silentdefault@gmail.com>
2017-09-14 14:02:05 -0500

    te7
    fucntional only with yours own signs

--- js/pichu.js
+++ js/pichu.js
@@ -1,5 +1,26 @@
+//principal functions
+'use strict';
 var pos = null;
 var server = 'http://mask.odacode.com';
+var dbAble = 'indexedDB' in window;
+var dbPromise = idb.open('PaS', 1, function (upgradeDb) {
+    if (!upgradeDb.objectStoreNames.contains('Signs')) {
+        upgradeDb.createObjectStore('Signs', { keyPath: "ID", autoIncrement: true });
+    }
+});
+///
+const radians = (deg) => deg * (Math.PI / 180);
+function getDistanceFromLatLonInKm(lat, lon) {
+    const RADIUS = 6371;
+    return RADIUS * Math.acos(
+        Math.sin(radians(lat)) *
+        Math.sin(radians(pos.latitude)) +
+        Math.cos(radians(lon - pos.longitude)) *
+        Math.cos(radians(lat)) *
+        Math.cos(radians(pos.latitude))
+    ); //return diference in km
+}
+///
 var frases = [
     "El(la) nunca volverá :'c", //0
     "El tamaño del mensaje depende de hace cuanto fue puesto el letrero", //1
@@ -23,61 +44,92 @@ function sw() {//serviceworker register
     }
 }
 
-function createSign() {
-    $.ajax({
-        url: server + '/pas.asmx/AddSign',
-        type: 'POST',
-        dataType: "json",
-        contentType: "application/json",
-        data: JSON.stringify({
-            s: {
-                Contenido: $('textarea').val(),
-                Latitud: pos.lat,
-                Longitud: pos.lon
-            }
-        }),
-    })
-        .done(function (data) {
-            if (data.d == 1) {
-                localStorage.setItem('Counter', Number(localStorage.getItem('Counter')) + 1);
-                $('textarea').val('');
-                $('textarea').keyup();
-                alert('Pinned!');
-            }
+function createSign() {//create sign
+    // if (navigator.onLine) {
+    //     $.ajax({
+    //         url: server + '/pas.asmx/AddSign',
+    //         type: 'POST',
+    //         dataType: "json",
+    //         contentType: "application/json",
+    //         data: JSON.stringify({
+    //             s: {
+    //                 Contenido: $('textarea').val(),
+    //                 Latitud: pos.lat,
+    //                 Longitud: pos.lon
+    //             }
+    //         }),
+    //     })
+    //         .done(function (data) {
+    //             if (data.d == 1) {
+    //                 localStorage.setItem('Counter', Number(localStorage.getItem('Counter')) + 1);
+    //                 $('textarea').val('');
+    //                 $('textarea').keyup();
+    //                 alert('Pinned!');
+    //             }
+    //         });
+    // }
+    dbPromise.then(db => {
+        const tx = db.transaction('Signs', 'readwrite');
+        tx.objectStore('Signs').put({
+            content: $('textarea').val(),
+            date: +new Date(),
+            latitude: pos.latitude,
+            longitude: pos.longitude
         });
+        return tx.complete;
+    }).then(() => {
+        $('textarea').val('').keyup();
+    });
+
 }
 
-function loadSigns() {
-    $.ajax({
-        url: server + 'mask.odacode.com/pas.asmx/LoadSign',
-        type: 'POST',
-        dataType: "json",
-        contentType: "application/json",
-        data: JSON.stringify({
-            Lat: pos.lat,
-            Lon: pos.lon
-        }),
-    })
-        .done(function (data) {
-            $('[signs]').empty();
-            data = data.d;
-            $.each(data, function (i, v) {
-                let tmp = 'h' + (moment().diff(moment(v.Fecha), 'days') + 1);
-                $('[signs]').append(`
-                    <sign>
-                        <${tmp} content>${v.Contenido}</${tmp}>
-                        <date>${moment(v.Fecha).format('MMMM DD YYYY, HH:mm')}</date>
-                    </sign>
-                `);
-            });
-        });
+function renderPost(p) {
+    $('signs').empty();
+    $.each(p, function (i, p) {
+        if (getDistanceFromLatLonInKm(p.latitude, p.longitude) < 1) {
+            let d = new Date(p.date);
+            $('signs').append(`
+                <sign d${p.ID}>
+                    <content d${Math.ceil((Math.abs(+Date() - p.date)) / (1000 * 3600 * 24))}>${p.content}</content>
+                    <date>${d.getUTCDate()}/${d.getMonth()}/${d.getUTCFullYear()}</date>
+                </sign>
+            `);
+        }
+    });
 }
 
-function eleResize() {
+function loadSigns() { //load signs
+    // if (navigator.onLine) {
+    //     $.ajax({
+    //         url: server + '/pas.asmx/LoadSign',
+    //         type: 'POST',
+    //         dataType: "json",
+    //         contentType: "application/json",
+    //         data: JSON.stringify({
+    //             Lat: pos.lat,
+    //             Lon: pos.lon
+    //         }),
+    //     })
+    //         .done(function (data) {
+    //             $('[signs]').empty();
+    //             data = data.d;
+    //             $.each(data, function (i, v) {
+    //                 renderPost(v);
+    //             });
+    //         });
+    // } else {
+    dbPromise.then(db => {
+        return db.transaction('Signs')
+            .objectStore('Signs').getAll();
+    }).then(v => renderPost(v));
+    //}
+}
+
+function eleResize() {//resize signs container
     $('signs').height($(window).height() - ($('editor').height() + (navigator.onLine ? 0 : $('offline').height())));
 }
 
-function txtArea() {
+function txtArea() {//textarea functionability
     var $this = $('textarea');
 
     if ($this.val().length >= 255) {
@@ -101,11 +153,7 @@ function txtArea() {
 $(document).ready(function () {
     sw();
     eleResize();
-    console.log($('textarea').height() + 'px');
     $('textarea').keyup(function () { txtArea(); });
-    $('offline').click(function () {
-        $('textarea').removeAttr('disabled')
-    });
     if (navigator.onLine) {
         workOnLine();
     } else {
@@ -123,6 +171,7 @@ function success(position) {
 Lon: ${pos.longitude}
 Signaler's Tips:
 "${frases[Math.floor(Math.random() * (frases.length - 0) + 0)]}"`);
+    loadSigns();
 };
 
 function error(err) {
@@ -132,31 +181,32 @@ Lon: -error-`);
 };
 
 if (navigator.geolocation) {
-    navigator.geolocation.watchPosition(success, error, {
-        timeout: 15000,
-        enableHighAccuracy: true
-    });
+    navigator.geolocation.watchPosition(success, error, { timeout: 15000, enableHighAccuracy: true });
 }
 else {
-    $('textarea').attr('placeholder', `Perdona`);
+    $('textarea').attr('placeholder', `Tu navegador o dispositivo no soporta la geolocalizacion`);
 }
 
 function workOnLine() {
     $('offline').css('z-index', '-1');
-    $('textarea').removeAttr('disabled');
+    $('textarea').removeAttr('disabled').keyup();
     eleResize();
 }
 
 function workOffLine() {
+    if (!dbAble) {
+        $('offline')
+            .css('background-color', 'red')
+            .css('color', 'white')
+            .text('Modo offline no compatible.');
+        $('textarea').attr('disabled', '');
+        $('button').attr('disabled', '');
+    }
     $('offline').css('z-index', '1');
     $('signs').height($(document).height() - ($('editor').height() + $('offline').height()));
     $('offline').css('bottom', $('editor').height() + 'px');
 }
 
-window.addEventListener('online', function (e) {
-    workOnLine();
-}, false);
+window.addEventListener('online', function (e) { workOnLine(); }, false);
 
-window.addEventListener('offline', function (e) {
-    workOffLine();
-}, false);
+window.addEventListener('offline', function (e) { workOffLine(); }, false);
